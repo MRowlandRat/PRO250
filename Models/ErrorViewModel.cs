92ea567d86d7182cfd8ccabd2828afc2cdc45d87
Michael Rowland <honeybadger448@gmail.com>
2023-08-22 12:48:44 -0600

    I MADE THE PROJECT

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP_Minesweeper.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
